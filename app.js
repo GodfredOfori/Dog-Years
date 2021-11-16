e09dbc01d7c1e7bb8d7dc5c73ca47ab61c2ba683
GodfredOfori <91945691+GodfredOfori@users.noreply.github.com>
2021-11-16 23:10:47 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const myAge = 23;
+//My age
+
+let earlyYears = 2;
+//Early years value
+earlyYears*= 10.5;
+
+let laterYears = myAge - 2;
+//Later years value 
+laterYears*= 4;
+//Dog years
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears + laterYears;
+//My dog age in years
+
+let myName = 'Godfred Ofori';
+myName.toLowerCase;
+//My name
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} old in dog years`);
+//My biography
